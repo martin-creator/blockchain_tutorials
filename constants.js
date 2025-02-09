ca838e70ab358e52c2524ab92c66a1ddf71d8237
martin-creator <martinlubowa@outlook.com>
2025-02-09 16:47:45 +0300

    Intial commit

--- constants.js
+++ constants.js
@@ -1,6 +1,8 @@
 // export const zkSyncContractAddress = "0x4B5DF730c2e6b28E17013A1485E5d9BC41Efe021"
 // export const contractAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
-export const contractAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
+export const contractAddress = "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0"
+
+
 export const abi = [
   {
     inputs: [
